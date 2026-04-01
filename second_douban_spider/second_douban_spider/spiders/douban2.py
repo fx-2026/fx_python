bcac7ccb089d2030bc7a068b9354bd9391d61ad1
fengxu <fengxu2013@163.com>
2026-04-01 23:09:36 +0800

    scrapy框架爬虫

---
+++ second_douban_spider/second_douban_spider/spiders/douban2.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class Douban2Spider(scrapy.Spider):
+    name = "douban2"
+    allowed_domains = ["example.com"]
+    start_urls = ["https://example.com"]
+
+    def parse(self, response):
+        print(response.url)
